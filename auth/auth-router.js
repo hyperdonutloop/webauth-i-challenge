48d66e2b81197667e20c635b1caba45f9d0a460f
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-16 15:39:18 -0800

    set up auth router, users model, and user router file

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,23 @@
+const bcrypt = require('bcryptjs');
+
 const router = require('express');
 
 const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
 
 module.exports = router;

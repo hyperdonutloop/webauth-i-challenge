606d207cb327f1512c558a3cbebc21edf3492331
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-16 15:15:04 -0800

    project set up and all dependencies added

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const apiRouter = require('./api-router.js');
+const configureMiddleware = require('./configure-middleware.js')
+
+const server = express();
+
+configureMiddleware(server);
+
+// server.use(express.json());
+server.use('/api', apiRouter);
+
+module.exports = server;
---
+++ auth/auth-router.js
@@ -0,0 +1,5 @@
+const router = require('express');
+
+const Users = require('../users/users-model.js');
+
+module.exports = router;
